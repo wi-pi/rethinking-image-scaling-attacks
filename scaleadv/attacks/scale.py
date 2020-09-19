33c62210aee888978110d9f7dfb321ebb51c743c
Yue Gao <11145354+Lodour@users.noreply.github.com>
2020-09-19 17:46:01 -0500

    Reformat scale attack

--- scaleadv/attacks/scale.py
+++ scaleadv/attacks/scale.py
@@ -1,35 +1,39 @@
 import numpy as np
-
-from scaling.ScalingGenerator import ScalingGenerator
-from scaling.SuppScalingLibraries import SuppScalingLibraries
-from scaling.SuppScalingAlgorithms import SuppScalingAlgorithms
+from PIL import Image
 from attack.QuadrScaleAttack import QuadraticScaleAttack
 from attack.adaptive_attack.AdaptiveAttackPreventionGenerator import AdaptiveAttackPreventionGenerator
 from defenses.detection.fourier.FourierPeakMatrixCollector import FourierPeakMatrixCollector, PeakMatrixMethod
-from defenses.prevention.PreventionDefenseType import PreventionTypeDefense
 from defenses.prevention.PreventionDefenseGenerator import PreventionDefenseGenerator
+from defenses.prevention.PreventionDefenseType import PreventionTypeDefense
+from scaling.ScalingGenerator import ScalingGenerator
+from scaling.SuppScalingAlgorithms import SuppScalingAlgorithms
+from scaling.SuppScalingLibraries import SuppScalingLibraries
+
+from scaleadv.datasets.imagenet import create_dataset
+
 
 class ScaleAttack(object):
 
-    def __init__(self, lib: SuppScalingLibraries, algo: SuppScalingAlgorithms, eps=1, bandwidth=2, allowed_changes=0.8, verbose=False):
+    def __init__(self, lib: SuppScalingLibraries, algo: SuppScalingAlgorithms, eps=1, bandwidth=2, allowed_changes=0.8,
+                 verbose=False):
         assert ScalingGenerator.check_valid_lib_alg_input(lib, algo)
         self.lib, self.algo = lib, algo
         self.eps, self.bw, self.modi = eps, bandwidth, allowed_changes
         self.verbose = verbose
 
     def generate(self, src: np.ndarray, tgt: np.ndarray):
-        self._validate_input(src)
-        self._validate_input(tgt)
+        self._validate(src)
+        self._validate(tgt)
 
         # scaling attack
-        scale_approach = ScalingGenerator.create_scaling_approach(src.shape, tgt.shape, self.lib, self.algo)
-        scale_attack = QuadraticScaleAttack(eps=self.eps, verbose=self.verbose)
-        scale_result, _, _ = scale_attack.attack(src, tgt, scale_approach)
+        scl_method = ScalingGenerator.create_scaling_approach(src.shape, tgt.shape, self.lib, self.algo)
+        scl_attack = QuadraticScaleAttack(eps=self.eps, verbose=self.verbose)
+        scl, _, _ = scl_attack.attack(src, tgt, scl_method)
 
         # adaptive scaling attack
         defense = PreventionDefenseGenerator.create_prevention_defense(
             defense_type=PreventionTypeDefense.medianfiltering,
-            scaler_approach=scale_approach,
+            scaler_approach=scl_method,
             fourierpeakmatrixcollector=FourierPeakMatrixCollector(PeakMatrixMethod.optimization, self.algo, self.lib),
             bandwidth=self.bw,
             verbose_flag=self.verbose,
@@ -37,24 +41,49 @@ class ScaleAttack(object):
         )
         adaptive_attack = AdaptiveAttackPreventionGenerator.create_adaptive_attack(
             defense_type=PreventionTypeDefense.medianfiltering,
-            scaler_approach=scale_approach,
+            scaler_approach=scl_method,
             preventiondefense=defense,
             verbose_flag=self.verbose,
             usecythonifavailable=True,
             choose_only_unused_pixels_in_overlapping_case=False,
             allowed_changes=self.modi
         )
-        scale_result_adaptive = adaptive_attack.counter_attack(scale_result)
+        ada = adaptive_attack.counter_attack(scl)
 
-        # apply median defense
-        scale_result_protected = defense.make_image_secure(scale_result)
-        scale_result_adaptive_protected = defense.make_image_secure(scale_result_adaptive)
+        return scl, ada, defense, scl_method
 
-        return scale_result, scale_result_protected, scale_result_adaptive, scale_result_adaptive_protected
-
-    def _validate_input(self, x: np.ndarray):
-        assert isinstance(x, np.ndarray)
+    def _validate(self, x: np.ndarray):
+        assert x.dtype == np.uint8
         assert x.ndim == 3
         assert x.shape[-1] == 3
-        assert x.min() >= 0.
-        assert x.max() > 1
+
+
+def test():
+    # load data
+    dataset = create_dataset(transform=None)
+    _, src, y_src = dataset[1000]
+    src = np.array(src, dtype=np.uint8)
+    _, tgt, y_tgt = dataset[2000]
+    tgt = np.array(tgt.resize((224, 224)), dtype=np.uint8)
+
+    # load attacker
+    lib = SuppScalingLibraries.PIL
+    algo = SuppScalingAlgorithms.NEAREST
+    attacker = ScaleAttack(lib, algo)
+
+    # attack
+    scl, ada, defense, _ = attacker.generate(src, tgt)
+
+    # defense
+    scl_def = defense.make_image_secure(scl)
+    ada_def = defense.make_image_secure(ada)
+
+    # save figs
+    caps = 'src', 'tgt', 'scl', 'scl_def', 'ada', 'ada_def'
+    for name in caps:
+        var = locals()[name]
+        Image.fromarray(var).save(f'{name}.png')
+
+
+if __name__ == '__main__':
+    test()
