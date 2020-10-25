8e0bb4cce29b13ee7f8d71090225f7353c22711a
Yue Gao <11145354+Lodour@users.noreply.github.com>
2020-10-24 23:09:02 -0500

    Add indirect Shadow attack

--- scaleadv/attacks/adv.py
+++ scaleadv/attacks/adv.py
@@ -1,16 +1,19 @@
 """
-This module implements the Projected Gradient Descent attack's invariants.
+This module implements several attacks' invariants.
 1. Attack x, compute perturbation on x.
-2. Attack x, compute perturbation on fixed augment(x).
+2. Attack x, compute perturbation on fixed proxy(x).
 
 Note:
-    Option 2 is implemented by hacking the estimator's `loss_gradient_framework` function.
-    This might be inaccurate due to projection on proxy(x) instead of x.
+    1. For PGD, option 2 is implemented by hacking the estimator's `loss_gradient_framework` function.
+       - This might be inaccurate due to projection on proxy(x) instead of x.
+       - This might be affected by future updates of PGD's official implementation.
 """
 from typing import Optional, Callable
 
 import numpy as np
 from art.attacks.evasion import ProjectedGradientDescentPyTorch, ShadowAttack
+from art.config import ART_NUMPY_DTYPE
+from tqdm import trange
 
 
 def loss_gradient_framework_average(super_call):
@@ -50,9 +53,56 @@ class IndirectPGD(ProjectedGradientDescentPyTorch):
         # Generate adversarial examples
         x_proxy_adv = super(IndirectPGD, self).generate(x_proxy, y_proxy)
         perturbation = x_proxy_adv[0] - x_proxy[0]
-        x_adv = np.clip(x + perturbation, *self.estimator.clip_values)
+        x_adv = np.clip(x + perturbation, *self.estimator.clip_values).astype(ART_NUMPY_DTYPE)
 
         # Recover gradient
         self.estimator.loss_gradient_framework = grad_fn
 
+        return x_adv
+
+
+class IndirectShadowAttack(ShadowAttack):
+
+    def generate(self, x: np.ndarray, y: Optional[np.ndarray] = None, proxy: Optional[Callable] = None) -> np.ndarray:
+        """
+        Generate adversarial example by attacking a proxy (not necessarily Normal Noise)
+        Args:
+            x: An array with the original inputs, shape is (1, C, H, W).
+            y: Target values.
+            proxy: A callable that returns a batch samples from x.
+
+        Returns:
+            An array holding the adversarial example.
+        """
+        if proxy is None:
+            return super(IndirectShadowAttack, self).generate(x, y)
+        if x.shape[0] != 1:
+            raise ValueError(f'Indirect attack only supports a single input, but got {x.shape[0]} inputs.')
+
+        # Set up proxies
+        x_proxy = proxy(x)
+        y_proxy = y.repeat(x_proxy.shape[0], axis=0)
+
+        # Initialize perturbation
+        perturbation = (
+                np.random.uniform(
+                    low=self.estimator.clip_values[0], high=self.estimator.clip_values[1], size=x.shape
+                ).astype(ART_NUMPY_DTYPE)
+                - (self.estimator.clip_values[1] - self.estimator.clip_values[0]) / 2
+        )
+
+        for _ in trange(self.nb_steps, desc="Shadow attack"):
+            gradients_ce = np.mean(
+                self.estimator.loss_gradient(x=x_proxy + perturbation, y=y_proxy, sampling=False)
+                * (1 - 2 * int(self.targeted)),
+                axis=0,
+                keepdims=True,
+            )
+            gradients = gradients_ce - self._get_regularisation_loss_gradients(perturbation)
+            perturbation += self.learning_rate * gradients
+
+        x_p = x + perturbation
+        x_adv = np.clip(x_p, a_min=self.estimator.clip_values[0], a_max=self.estimator.clip_values[1]).astype(
+            ART_NUMPY_DTYPE
+        )
         return x_adv
