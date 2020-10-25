36fff699ee7284377812870651e873059dcbd04c
Yue Gao <11145354+Lodour@users.noreply.github.com>
2020-10-24 22:52:31 -0500

    Add indirect PGD attack

--- scaleadv/attacks/adv.py
+++ scaleadv/attacks/adv.py
@@ -1,87 +1,58 @@
+"""
+This module implements the Projected Gradient Descent attack's invariants.
+1. Attack x, compute perturbation on x.
+2. Attack x, compute perturbation on fixed augment(x).
+
+Note:
+    Option 2 is implemented by hacking the estimator's `loss_gradient_framework` function.
+    This might be inaccurate due to projection on proxy(x) instead of x.
+"""
+from typing import Optional, Callable
+
 import numpy as np
-import torch.nn as nn
-import torchvision.transforms as T
-from art.attacks.attack import EvasionAttack
-from art.attacks.evasion import ShadowAttack
-from art.estimators.classification import PyTorchClassifier
-from torch.nn import DataParallel
-from torchvision.models import resnet50
-
-from scaleadv.datasets.imagenet import create_dataset, IMAGENET_MEAN, IMAGENET_STD
-from scaleadv.models.layers import NormalizationLayer
+from art.attacks.evasion import ProjectedGradientDescentPyTorch, ShadowAttack
 
 
-def _to_batch(x: np.ndarray):
-    # [0, 255](W, H, C) to [0, 1](1, C, W, H) in float32
-    x = x.astype(np.float32) / 255.
-    x = x.transpose((2, 0, 1))[None, ...]
-    return x
+def loss_gradient_framework_average(super_call):
+    def wrapper(x, y, **kwargs):
+        grad = super_call(x, y, **kwargs)
+        return grad.mean(0).repeat(x.shape[0], 1, 1, 1)
+
+    return wrapper
 
 
-def _to_single(x: np.ndarray):
-    # [0, 1](1, C, W, H) to [0, 255](W, H, C) in uint8
-    x = x[0].transpose((1, 2, 0)) * 255.
-    x = x.round().astype(np.uint8)
-    return x
+class IndirectPGD(ProjectedGradientDescentPyTorch):
 
+    def generate(self, x: np.ndarray, y: Optional[np.ndarray] = None, proxy: Optional[Callable] = None) -> np.ndarray:
+        """
+        Generate adversarial example by attacking a proxy.
+        Args:
+            x: An array with the original inputs, shape is (1, C, H, W).
+            y: Target values.
+            proxy: A callable that returns a batch samples from x.
 
-class AdvAttack(object):
+        Returns:
+            An array holding the adversarial example.
+        """
+        if proxy is None:
+            return super(IndirectPGD, self).generate(x, y)
+        if x.shape[0] != 1:
+            raise ValueError(f'IndirectPGD only supports a single input, but got {x.shape[0]} inputs.')
 
-    def __init__(self, classifier: PyTorchClassifier, attacker: EvasionAttack):
-        assert isinstance(classifier, PyTorchClassifier)
-        assert isinstance(attacker, EvasionAttack)
-        self.classifier = classifier
-        self.attacker = attacker
+        # Set up proxies
+        x_proxy = proxy(x)
+        y_proxy = y.repeat(x_proxy.shape[0], axis=0)
 
-    def predict(self, x: np.ndarray):
-        self._validate(x)
-        x = _to_batch(x)
-        pred = self.classifier.predict(x)
-        return pred.argmax(1)[0], pred
+        # Hack gradient
+        grad_fn = self.estimator.loss_gradient_framework
+        self.estimator.loss_gradient_framework = loss_gradient_framework_average(grad_fn)
 
-    def generate(self, x: np.ndarray, target: np.ndarray = None):
-        self._validate(x)
-        x = _to_batch(x)
-        if target is not None:
-            x_adv = self.attacker.generate(x, target)
-        else:
-            x_adv = self.attacker.generate(x)
-        return _to_single(x_adv)
+        # Generate adversarial examples
+        x_proxy_adv = super(IndirectPGD, self).generate(x_proxy, y_proxy)
+        perturbation = x_proxy_adv[0] - x_proxy[0]
+        x_adv = np.clip(x + perturbation, *self.estimator.clip_values)
 
-    def _validate(self, x: np.ndarray):
-        assert x.dtype == np.uint8
-        assert x.ndim == 3
-        assert x.shape[::-1] == self.classifier.input_shape
+        # Recover gradient
+        self.estimator.loss_gradient_framework = grad_fn
 
-
-def test():
-    # load data
-    dataset = create_dataset(transform=T.Resize((224, 224)))
-    _, x, y = dataset[5000]
-    x = np.array(x)
-
-    # load classifier
-    model = nn.Sequential(NormalizationLayer(IMAGENET_MEAN, IMAGENET_STD), resnet50(pretrained=True)).eval()
-    model = DataParallel(model).cuda()
-    classifier = PyTorchClassifier(model, nn.CrossEntropyLoss(), (3, 224, 224), 1000, clip_values=(0., 1.))
-
-    # load attacker
-    # TODO: Support targeted attack.
-    # attacker = ProjectedGradientDescentPyTorch(classifier, np.inf, 0.03, 0.007, max_iter=10)
-    attacker = ShadowAttack(classifier, sigma=0.2, lambda_tv=0.3, lambda_s=0.5)
-    aa = AdvAttack(classifier, attacker)
-
-    # evaluate
-    x_evil = aa.generate(x)
-    y_pred, _ = aa.predict(x)
-    y_evil, _ = aa.predict(x_evil)
-    print('True', y)
-    print('Pred', y_pred)
-    print('Evil', y_evil)
-
-    from PIL import Image
-    Image.fromarray(x_evil).save('shadow.png')
-
-
-if __name__ == '__main__':
-    test()
+        return x_adv
