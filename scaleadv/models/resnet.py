5a7f883f8d563c133611812ec4fda69eb57f20ca
Yue Gao <11145354+Lodour@users.noreply.github.com>
2020-11-18 14:30:05 -0600

    Add smoothing models

--- scaleadv/models/resnet.py
+++ scaleadv/models/resnet.py
@@ -6,6 +6,8 @@ import torchvision.models as models
 IMAGENET_MODEL_PATH = {
     'inf': 'static/models/imagenet_linf_4.pt',
     '2': 'static/models/imagenet_l2_3_0.pt',
+    'smooth': 'static/models/models/imagenet/resnet50/noise_1.00/checkpoint.pth.tar',
+    'smooth-adv': 'static/models/pretrained_models/imagenet/PGD_1step/imagenet/eps_255/resnet50/noise_1.00/checkpoint.pth.tar'
 }
 
 
@@ -15,8 +17,15 @@ def resnet50_imagenet(robust: Optional[str] = None):
 
     if robust is not None:
         assert robust in IMAGENET_MODEL_PATH
-        prefix = 'module.model.'
-        ckpt = torch.load(IMAGENET_MODEL_PATH[robust]).get('model', {})
+        if robust in ['2', 'inf']:
+            prefix = 'module.model.'
+            key = 'model'
+        elif robust in ['smooth', 'smooth-adv']:
+            prefix = '1.module.'
+            key = 'state_dict'
+        else:
+            raise NotImplementedError
+        ckpt = torch.load(IMAGENET_MODEL_PATH[robust]).get(key, {})
         ckpt = {k.replace(prefix, ''): v for k, v in ckpt.items() if k.startswith(prefix)}
         network.load_state_dict(ckpt)
 
