15d560d2799a68e58c556fb7ab9d1bbfe035423e
Yue Gao <11145354+Lodour@users.noreply.github.com>
2020-12-12 22:27:53 -0600

    new experiments: acc-vs-budget

---
+++ scaleadv/tests/plot_acc_vs_budget.py
@@ -0,0 +1,197 @@
+"""
+This module implements experiments that compare Accuracy vs Attack Budget.
+"""
+import os
+import pickle
+from collections import defaultdict
+from typing import List, Optional
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy.stats
+from torch.utils.data import DataLoader, Subset
+from tqdm import tqdm
+
+from scaleadv.tests.scale_adv import *
+
+
+def get_dataset_by_ratio(ratio: int, nb_data: Optional[int] = None):
+    root = f'static/datasets/imagenet-ratio/{ratio}'
+    t = T.Compose([
+        lambda img: resize_to_224x(img, scale=ratio, square=True),
+        T.ToTensor(),
+        lambda x: x.numpy(),
+    ])
+    dataset = create_dataset(root=root, transform=t)
+    if nb_data is not None:
+        tot = len(dataset)
+        dataset = Subset(dataset, list(range(0, tot, tot // nb_data))[:nb_data])
+    return dataset
+
+
+class AccBudget(object):
+    # PGD params
+    NORM = 2
+    STEP = 30
+    BIG_STEP = 100
+    NB_DATA = 100
+
+    def __init__(self, class_net: nn.Module, target: int, lib: str, algo: str, tag: str):
+        self.class_net = class_net
+        self.target = target
+        self.lib = LIB_TYPE[lib]
+        self.algo = ALGO_TYPE[algo]
+        self.tag = tag
+        self.saved_results = defaultdict(list)
+
+    def eval_ratio_eps(self, ratio: int, eps: int, defense: str = 'none', mode: str = None,
+                       dump: bool = True, load: bool = False):
+        fname = f'eval-dataset.{ratio}.{eps:03d}.{defense}.{mode}.pkl'
+        if load:
+            if os.path.exists(fname):
+                print(fname)
+                return pickle.load(open(fname, 'rb'))
+            return None
+
+        # Load data
+        dataset = get_dataset_by_ratio(ratio, nb_data=self.NB_DATA)
+        loader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=8)
+        src_size = (224 * ratio,) * 2 + (3,)
+        inp_size = INPUT_SHAPE_PIL
+
+        # Load scaling algorithm
+        scaling = ScalingGenerator.create_scaling_approach(src_size, inp_size, self.lib, self.algo)
+        mask = get_mask_from_cl_cr(scaling.cl_matrix, scaling.cr_matrix)
+
+        # Get pooling layer
+        kernel = src_size[0] // inp_size[0] * 2 - 1
+        pooling_args = kernel, 1, kernel // 2, mask
+        pooling = POOLING[defense](*pooling_args)
+        nb_samples = 1 if mode is None else 128
+
+        # Get networks
+        scale_net = ScaleNet(scaling.cl_matrix, scaling.cr_matrix).eval()
+        class_net = self.class_net.eval()
+        if nb_samples > 1:
+            class_net = BalancedDataParallel(FIRST_GPU_BATCH, class_net)
+        scale_net = scale_net.cuda()
+        class_net = class_net.cuda()
+
+        # Get art's proxy
+        classifier = PyTorchClassifier(class_net, nn.CrossEntropyLoss(), INPUT_SHAPE_NP, NUM_CLASSES,
+                                       clip_values=(0, 1))
+        y_target = np.eye(NUM_CLASSES, dtype=int)[None, self.target]
+        eps_step = 2.5 * eps / self.STEP
+        adv_attack = IndirectPGD(classifier, self.NORM, eps, eps_step, self.STEP, targeted=True, verbose=False)
+
+        # Get scaling attack
+        scl_attack = ScaleAttack(scale_net, class_net, pooling)
+        attack_args = dict(norm=self.NORM, eps=eps * ratio, eps_step=4.0 * eps / self.STEP, max_iter=self.BIG_STEP,
+                           targeted=True, batch_size=NUM_SAMPLES_PROXY, verbose=False)
+
+        # Eval all data
+        e = Evaluator(scale_net, class_net, pooling_args, nb_samples=nb_samples)
+        desc = f'Evaluate (ratio {ratio}, eps {eps}, defense {defense})'
+        data = []
+        with tqdm(loader, desc=desc) as pbar:
+            for i, (src, y) in enumerate(pbar):
+                # get input
+                inp = scale_net(src.cuda()).cpu().numpy()
+                src = src.cpu().numpy()
+                # get adv
+                adv = adv_attack.generate(inp, y_target)
+                # get att
+                att = scl_attack.generate(src, self.target, IndirectPGD, attack_args, mode=mode, nb_samples=nb_samples,
+                                          verbose=False)
+                # eval
+                stats = e.eval(src, adv, att, y_adv=self.target)
+                data.append(stats)
+
+                if dump:
+                    pickle.dump(data, open(fname, 'wb'))
+
+        return data
+
+    def get_acc(self, data: List, img_field: str = 'SRC', y_field: str = 'Y'):
+        cnt = 0
+        for stat in data:
+            pred = scipy.stats.mode(stat[img_field][y_field])[0]
+            if stat['SRC']['Y'] == pred:
+                cnt += 1
+        acc = cnt / len(data) * 100
+        return acc
+
+    def collect(self, data: List, eps: int, tag: str, plot: bool = True):
+        # Compute acc
+        self.saved_results['ADV'].append((eps, self.get_acc(data, 'ADV', 'Y')))
+        self.saved_results['ATT'].append((eps, self.get_acc(data, 'ATT', tag)))
+        if plot:
+            self.plot(tag)
+
+    def plot(self, tt):
+        plt.figure()
+        for tag in ['ADV', 'ATT']:
+            eps, acc = zip(*self.saved_results[tag])
+            plt.plot(eps, acc, label=tag.lower())
+        plt.legend()
+        plt.savefig(f'test-{tt}.pdf')
+
+
+def plot_all():
+    defenses = {'none': 'Y', 'median': 'Y_MED', 'random': 'Y_RND'}
+    plt.figure(constrained_layout=True)
+
+    for defense, tag in defenses.items():
+        tester = AccBudget(class_net, args.target, lib='cv', algo='linear', tag='TEST')
+        for eps in range(1, 26):
+            mode = 'cheap' if defense == 'random' else None
+            d = tester.eval_ratio_eps(args.ratio, eps=eps, defense=defense, mode=mode, dump=False, load=True)
+            if d is not None and len(d) == 100:
+                tester.collect(d, eps, tag, plot=False)
+        # plot adv
+        if defense == 'none':
+            eps, acc = zip(*tester.saved_results['ADV'])
+            plt.plot(eps, acc, marker='o', markersize=3, label='PGD Attack')
+        # plot att
+        eps, acc = zip(*tester.saved_results['ATT'])
+        plt.plot(eps, acc, marker='o', markersize=3, label=f'Scale-Adv Attack ({defense})')
+
+    plt.ylim(-5, 105)
+    plt.legend()
+    plt.yticks(list(range(0, 101, 10)))
+    plt.savefig('gen_acc-vs-L2.pdf')
+
+
+if __name__ == '__main__':
+    # set font for ccs
+    mpl.rcParams['font.sans-serif'] = "Linux Libertine"
+    mpl.rcParams['font.family'] = "sans-serif"
+    mpl.rcParams['font.size'] = 13
+
+    p = ArgumentParser()
+    # Input args
+    p.add_argument('--target', type=int, required=True)
+    p.add_argument('--model', default=None, type=str, choices=ROBUST_MODELS)
+    # Scaling args
+    p.add_argument('--ratio', type=int, choices=(2, 3, 4, 5, 6))
+    p.add_argument('--defense', type=str)
+    # Misc
+    p.add_argument('--load', action='store_true')
+    p.add_argument('--plot', action='store_true')
+    args = p.parse_args()
+
+    # Load networks
+    class_net = nn.Sequential(NormalizationLayer.from_preset('imagenet'), resnet50_imagenet(args.model)).eval()
+
+    # plot all
+    if args.plot:
+        plot_all()
+        exit()
+
+    # attack
+    tester = AccBudget(class_net, args.target, lib='cv', algo='linear', tag='TEST')
+    for eps in range(1, 26):
+        mode = 'cheap' if args.defense == 'random' else None
+        d = tester.eval_ratio_eps(args.ratio, eps=eps, defense=args.defense, mode=mode, dump=True, load=args.load)
+        tag = {'random': 'Y_RND', 'median': 'Y_MED', 'none': 'Y'}
+        tester.collect(d, eps, tag[args.defense])
