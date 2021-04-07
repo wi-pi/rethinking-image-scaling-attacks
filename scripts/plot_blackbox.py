8246061bc42ee2db738acab529ded22532bc07e2
Yue Gao <11145354+Lodour@users.noreply.github.com>
2021-04-07 10:30:28 -0500

    minor update

--- scripts/plot_blackbox.py
+++ scripts/plot_blackbox.py
@@ -1,18 +1,20 @@
-import pickle
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import seaborn as sns
-import pandas as pd
 import os
+import pickle
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
 
 from scaleadv.utils import set_ccs_font
 
 BINS = [1000 * i for i in range(1, 26)]
 
+
 def get_one(name):
     if os.path.exists(name):
         return pickle.load(open(name, 'rb'))
     raise NotImplementedError
+
 
 def to_bins(data):
     results = [0] * len(BINS)
@@ -29,7 +31,7 @@ def get(i):
     try:
         vanilla = get_one(f'static/bb/{i}.ratio_1.def_none.log')
         scale_none = get_one(f'static/bb/{i}.ratio_3.def_none.log')
-        scale_med = get_one(f'static/bb/{i}.ratio_3.def_median.log')
+        scale_med = get_one(f'static/bb_med37/{i}.ratio_3.def_median.log')
     except NotImplementedError:
         return None
 
@@ -54,15 +56,12 @@ if __name__ == '__main__':
     BLUE, ORANGE, GREEN, RED = plt.rcParams['axes.prop_cycle'].by_key()['color'][:4]
     plt.figure(figsize=(3, 3), constrained_layout=True)
     plt.plot(x, vanilla.median(), marker='D', ms=4, lw=1.5, c='k', label='HopSkipJump')
-    plt.plot(x, scale_none.median() / 3, marker='o', ms=4, lw=1.5, c=GREEN, label='Scale-Adv (None)')
-    plt.plot(x, scale_med.median() / 3, marker='^', ms=4, lw=1.5, c=ORANGE, label='Scale-Adv (Median)')
+    plt.plot(x, scale_none.median() / 3, marker='o', ms=4, lw=1.5, c=GREEN, label='Scale-Adv (none)')
+    plt.plot(x, scale_med.median() / 3, marker='^', ms=4, lw=1.5, c=ORANGE, label='Scale-Adv (median)')
     plt.legend(borderaxespad=0.5)
     plt.yscale('log')
     plt.xticks(list(range(0, 26, 5)), [f'{i}K' for i in range(0, 26, 5)])
     plt.xlabel('Number of Queries')
-    plt.ylabel(r'$\ell_2$ Distortion')
+    plt.ylabel(r'Distance ($\ell_2$)')
     plt.grid(True)
     plt.savefig(f'l2-vs-queries.pdf')
-
-
-
