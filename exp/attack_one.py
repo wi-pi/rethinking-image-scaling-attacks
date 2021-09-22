aaafbefc88bab5386c2547d1ff257c145c36f56c
Yue Gao <11145354+Lodour@users.noreply.github.com>
2021-09-22 01:07:07 -0500

    update exp

--- exp/attack_one.py
+++ exp/attack_one.py
@@ -1,14 +1,18 @@
 import argparse
 
 import numpy as np
+import torch.nn as nn
 import torchvision.transforms as T
+from art.attacks.evasion import ProjectedGradientDescentPyTorch
+from art.estimators.classification import PyTorchClassifier
 from loguru import logger
 
+from exp.utils import savefig
 from scaleadv.datasets import get_imagenet
 from scaleadv.datasets.transforms import Align
-from scaleadv.models.resnet import IMAGENET_MODEL_PATH
-from scaleadv.scaling import ScalingAPI
-from scaleadv.scaling.enum import str_to_alg, str_to_lib
+from scaleadv.models import ScalingLayer
+from scaleadv.models.resnet import IMAGENET_MODEL_PATH, resnet50
+from scaleadv.scaling import *
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -40,8 +44,30 @@ if __name__ == '__main__':
     logger.info(f'Load source image: id {args.id}, label {y_large}, shape {x_large.shape}, dtype {x_large.dtype}.')
 
     # Load scaling api
-    api = ScalingAPI(x_large.shape[-2:], (256, 256), args.lib, args.alg)
-    x_small = api(x_large[0])[None]
+    shape_large = x_large.shape[-2:]
+    shape_small = (256, 256)
+    api = ScalingAPI(src_shape=x_large.shape[-2:], tgt_shape=(256, 256), lib=args.lib, alg=args.alg)
+    # x_small = api(x_large[0])[None]
 
-    print(x_large.shape)
-    print(x_small.shape)
+    # Load network
+    scaling_layer = ScalingLayer.from_api(api)
+    backbone_network = resnet50(args.model, normalize=True)
+    model = nn.Sequential(scaling_layer, backbone_network)
+    classifier = PyTorchClassifier(model, nn.CrossEntropyLoss(), x_large.shape[1:], 1000, clip_values=(0, 1))
+
+    # Load attack
+    attack = ProjectedGradientDescentPyTorch(
+        classifier,
+        norm=2,
+        eps=args.eps,
+        eps_step=args.eps * 2.5 / args.step,
+        max_iter=args.step,
+        targeted=False,
+        verbose=False,
+    )
+
+    # Run attack
+    adv_large = attack.generate(x_large, np.eye(1000)[[y_large]])
+    adv_small = api(adv_large[0])
+    savefig(adv_large, 'test1.png')
+    savefig(adv_small, 'test2.png')
--- scaleadv/scaling/__init__.py
+++ scaleadv/scaling/__init__.py
@@ -1,2 +1,2 @@
 from scaleadv.scaling.api import ScalingAPI
-from scaleadv.scaling.enum import ScalingAlg, ScalingLib
+from scaleadv.scaling.enum import ScalingAlg, ScalingLib, str_to_lib, str_to_alg
