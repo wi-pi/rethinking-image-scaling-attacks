87132d3730832bbdf4e72243a8dd21b6ebf1ab22
Yue Gao <11145354+Lodour@users.noreply.github.com>
2022-04-08 10:19:46 -0500

    feat: move online model to numpy

--- scaleadv/models/api.py
+++ scaleadv/models/api.py
@@ -7,6 +7,7 @@ import numpy as np
 import torch
 import torchvision.transforms.functional as F
 from PIL import Image
+from loguru import logger
 from tencentcloud.common import credential
 from tencentcloud.tiia.v20190529 import tiia_client, models
 from tencentcloud.tiia.v20190529.models import DetectLabelItem
@@ -18,22 +19,32 @@ class OnlineModel(object):
     def __init__(self):
         cred = credential.Credential(self.ID, self.KEY)
         self.client = tiia_client.TiiaClient(cred, region='ap-shanghai')
+        self.current_label = None
+        self.nb_query = 0
 
-    def predict(self, x: torch.Tensor, y_true: str) -> np.ndarray:
+    def set_current_sample(self, x: np.ndarray) -> Optional[str]:
+        true_label = self.get_true_label(x)
+        self.current_label = true_label
+        self.nb_query = 0
+        logger.debug(f'Get true label {true_label}')
+        return true_label
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
         # Query
         prediction = self.query(x)
+        self.nb_query += 1
 
         # Get confidence of true label
         pos_score = 0
         for pred in prediction:
-            if pred.Name == y_true:
+            if pred.Name == self.current_label:
                 pos_score = pred.Confidence
                 break
 
         # Get confidence of a different label
         neg_score = np.inf
         for pred in prediction:
-            if pred.Name != y_true:
+            if pred.Name != self.current_label:
                 neg_score = pred.Confidence
                 break
 
@@ -41,14 +52,19 @@ class OnlineModel(object):
         output = np.array([[neg_score, pos_score]])
         return output
 
-    def get_true_label(self, x: torch.Tensor) -> Optional[str]:
+    def get_true_label(self, x: np.ndarray) -> Optional[str]:
         label = self.query(x)[0]
-        return label.Name if label.Confidence > 50 else None
+        return label.Name if label.Confidence > 20 else None
 
-    def query(self, x: torch.Tensor) -> List[DetectLabelItem]:
+    def query(self, x: np.ndarray) -> List[DetectLabelItem]:
+        if x.ndim == 4:
+            if x.shape[0] != 1:
+                raise ValueError(f'Does not support batch size > 1, got {x.shape}.')
+            x = x[0]
+
         # To PIL
         buf = io.BytesIO()
-        F.to_pil_image(x).save(buf, format='png')
+        F.to_pil_image(torch.from_numpy(x)).save(buf, format='png')
 
         # Send request
         req = models.DetectLabelRequest()
@@ -56,8 +72,10 @@ class OnlineModel(object):
         req.Scenes = ['CAMERA']
 
         # Get respond labels
-        resp = self.client.DetectLabel(req)
-        return resp.CameraLabels
+        prediction = self.client.DetectLabel(req).CameraLabels
+        msg = ', '.join(f'{pred.Name} {pred.Confidence}' for i, pred in enumerate(prediction) if i < 5)
+        logger.debug(msg)
+        return prediction
 
 
 if __name__ == '__main__':
@@ -70,7 +88,7 @@ if __name__ == '__main__':
     ]
 
     for name in file_list:
-        x = F.to_tensor(Image.open(name))
-        y_true = model.get_true_label(x)
-        y_pred = model.predict(x, y_true)
+        x = F.to_tensor(Image.open(name)).numpy()
+        y_true = model.set_current_sample(x)
+        y_pred = model.predict(x)
         print(y_true, y_pred)
